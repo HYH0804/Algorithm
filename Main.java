ec884b54533e22ecbef54dde99ec52421cdc4b29
HYH0804 <guswnsgur2276@gmail.com>
2023-04-14 23:22:03 +0900

    try hard

---
+++ Main.java
@@ -0,0 +1,63 @@
+
+
+public class Main  {
+	
+	public static void main(String[] args) {
+		String[] words = {"write","program","that","receive","positive"};
+		
+		String anotherGame;
+		
+		
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		
+		
+		do { 
+			java.util.Random rand = new java.util.Random();
+	        int random = rand.nextInt(5);
+			char[] strToArray = words[random].toCharArray(); //문자열 배열로 바꾸고
+			int count = strToArray.length; //str의 글자 개수
+			char[] current_guess = new char[count]; //현재까지 맞춘 
+			for (int z = 0; z< count; z++) {
+				current_guess[z]= '*';
+			}
+			int remain = count; //남은 알파벳
+			int try_count =0; // 시도횟수
+			while (remain != 0) { //글씨 다 맞출때까지...
+				System.out.println("(Guess) Enter a letter in word "+String.valueOf(current_guess)+"> ");
+				char guess = input.next().charAt(0);
+				int remain_before = remain; 
+				for (int i = 0 ; i<words[random].length(); i++) { // 입력값을 배열 인덱스 0 부터 순회하며 같은 것 있는지 확인
+					
+					if(words[random].charAt(i)==guess) { //i에서 맞음
+						current_guess[i] = guess;
+						remain--; //남은 알파벳 감소
+					}
+					else
+						continue; //순회하면서 각 index에서 같지 않으면 다시 continue
+					
+				}
+				if (remain_before == remain) {
+					try_count++;
+					System.out.printf("%c is not in the word\n", guess); 
+				}
+			}
+			System.out.printf("The word is %s. You missed %d times\n", words[random] , try_count );
+			System.out.printf("Do you want to guess for another word? Enter y or n > ");
+			
+		} while (input.nextLine()=="y"); //다른 str로 행맨 할때까지
+	}
+}
+	/*
+int check_guess(String str , char guess , int remain , char[] current_guess) {
+for (int i = 0 ; i<str.length(); i++) { // 입력값을 배열 인덱스 0 부터 순회하며 같은 것 있는지 확인
+	
+	if(str.charAt(i)==guess) { //i에서 맞음
+		current_guess[i] = guess;
+		remain--; //남은 알파벳 감소
+	}
+	else
+		continue; //순회하면서 각 index에서 같지 않으면 다y시 continue
+	
+	}
+}
+	*/
---
+++ hi.cpp
@@ -0,0 +1,181 @@
+template <class T>
+void ChangeSize1D(T*&a , const int oldSize , const int newSize)
+{
+    if(newSize < 0) throw "Error";
+    T * temp = new T[newSize];
+    int number = min(oldSize , newSize);
+    copy(a,a+number,temp);
+    delete[] a;
+    a=temp;
+}
+---------------------------------------------------------------------------------------------
+template <class T>
+class Bag {
+    public:
+        Bag(int bagcapacity=10);
+        ~Bag();
+        bool IsEmpty() ;
+        int Size();
+        T& Element();
+        void Push(const T&);
+        void Pop();
+    private:
+        int capacity;
+        T *array;
+        int top;
+}
+
+template <class T>
+Bag<T>::Bag(int bagcapacity=10): capacity(bagcapacity) {
+    if(bagcapacity<1) throw "Error";
+    array = new T[capacity];
+    top = -1;
+}
+
+template <class T>
+Bag<T>::IsEmpty() { 
+    return top == -1; 
+}
+
+template <class T>
+int Bag<T>::Size(){
+    return top;
+}
+
+template <class T>
+T& Bag<T>::Element(){
+    if(IsEmpty()) throw "Error";
+    return array[0];
+}
+
+template <class T>
+void Bag<T>::Push(const T& obj){
+    if(top == capacity-1) 
+        ChangeSize1D(array,capacity,capacity*2);
+    capacity*=2;
+    array[++top]=obj;
+    
+
+}
+
+template <class T>
+void Bag<T>::Pop(){
+    if(IsEmpty()) throw "Error";
+    int deletePos = top/2;
+    copy(array+deletePos + 1 , array + top +1 , array+deletePos);
+    top--;
+}
+-------------------------------------------------------------------------------------------------
+
+template <class T>
+class Stack{
+    public:
+        Stack(int stackCapacity=10): capacity(stackCapacity) ;
+        bool IsEmpty();
+        T& Top();
+        void Pop();
+        void Push(const T&);
+    private:
+        int capacity;
+        int top;
+        T * array;
+
+        
+}
+
+template <class T>
+Stack<T>::Stack(int stackCapacity=10): capacity(stackCapacity){
+        if(stackCapacity<1) throw "Error";
+        array = new T[capacity];
+        top = -1;
+}
+
+template <class T>
+bool Stack<T>::IsEmpty()
+    return top==-1;
+
+template <class T>
+T& Stack<T>:: Top() {
+    if(IsEmpty()) throw "Error";
+    return array[top];
+}
+
+template <class T>
+void Stack<T>::Pop(){
+    if(IsEmpty()) throw "Error";
+    array[top--].~T(); 
+}
+
+template <class T>
+void Stack<T>::Push(const T& obj){
+    if(top == capacity-1) {
+        Changesize1D(stack,capacity,2*capacity);
+        capacity *=2;
+    }
+    stack[++top]=obj;
+}
+
+-------------------------------------------------------------------------------------
+
+template <class T>
+class Queue {
+    public:
+        Queue(int queueCapacity=10);
+        T& front(); 
+        T& rear();
+        bool IsEmpty();
+        void Pop();
+        void Push(const T&);
+    private:
+        T * array;
+        int capacity;
+        int rear;
+        int front;
+}
+template <class T>
+Queue<T>::Queue(int queueCapacity=10): capacity(queueCapacity){
+    if(queueCapacity<1) throw "Error";
+    array = new T[capacity];
+    front = rear = 0;
+}
+
+template <class T>
+bool Queue<T>::IsEmpty() { return front == rear };
+
+template <class T>
+T& Queue<T>::front(){
+    if(IsEmpty()) throw "Error";
+    return array[(front+1)%capacity];
+}
+
+template <class T>
+T& Queue<T>::rear(){
+    if(IsEmpty()) throw "Error";
+    return array[rear];
+}
+
+void Queue<T>::Pop(){
+    if(IsEmpty()) throw "Error";
+    front= (front+1)%capacity;
+    array[front].~T();
+    
+}
+// ㅅㅂ 킵
+void Queue<T>::Push(const T & obj){
+    if(((rear+1)%capacity) == front )
+    {//큐가 만원이면 크기 두배로 만드는 코드
+        T * newQueue = new T[2*capacity];
+
+        int start = (front+1)%capacity;
+        if(start<2)
+            copy(array+start,array+start+capacity-1 , newQueue);
+        else 
+        {
+            copy(array+start,array+capacity,newQueue);
+            copy(array,array+rear+1,newQueue+capacity-start);
+        }
+
+     }
+    rear = (rear+1)%capacity;
+    array[rear]=obj;
+}
