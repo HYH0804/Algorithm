87edb298a2b5d0ad6440b111700ba787ebbe8734
준혁 <127603139+HYH0804@users.noreply.github.com>
2023-04-19 17:46:05 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath> //pow(a,b) a^b 리턴형 double / sqrt 
+using namespace std;
+
+int isOrder(int a[]);
+
+int main(int argc, char *argv[])
+{
+	int a,b;
+	int new_a,new_b;
+	cin>> a >>b;
+	int arr_a[3]={0,};
+	int arr_b[3]={0,};
+	for (int i=0; i<3; i++){
+		arr_a[2-i]=(int)(a/pow(10,2-i))%10; //정수형 각자리 수 분해 
+		arr_b[2-i]=(int) (b/pow(10,2-i))%10;
+	}
+	new_a = arr_a[0]*100+arr_a[1]*10+arr_a[2];
+	new_b = arr_b[0]*100+arr_b[1]*10+arr_b[2];
+	
+	if (new_a>new_b){
+		cout<<new_a;
+		}
+		else
+		cout<<new_b;
+}
