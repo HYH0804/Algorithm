31961143c1dbe0d9f179fcbe94e9146ea6a68325
준혁 <127603139+HYH0804@users.noreply.github.com>
2023-04-19 17:52:00 +0900

    Update 상수.cc

--- 백준/Bronze/2908. 상수/상수.cc
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
 	for (int i=0; i<3; i++){
 		arr_a[2-i]=(int)(a/pow(10,2-i))%10; //정수형 각자리 수 분해 
 		arr_b[2-i]=(int) (b/pow(10,2-i))%10;
+		/*
+		arr_a[i] = a % 10 ;
+		a /= 10;
+		arr_b[i] = b % 10;
+		b /= 10;
+		*/
 	}
 	new_a = arr_a[0]*100+arr_a[1]*10+arr_a[2];
 	new_b = arr_b[0]*100+arr_b[1]*10+arr_b[2];
