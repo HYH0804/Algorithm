288bbf8c3a0e82ae27fd4692377e343b342d0758
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-11-20 18:49:47 +0900

    [Silver V] Title: K번째 수, Time: 1056 ms, Memory: 368340 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,29 +1,74 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-//		int[] A = new int[N]; // 풀이 3
-//		List<Integer> A = new LinkedList<>(); // 풀이 2
-		List<Integer> A = new ArrayList<>(); // 풀이 1
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		Collections.sort(A);
-		
-		System.out.println(A.get(K-1));
-
-	}
-
+    
+    static int a[];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        quickSort(a, 0, n - 1, k - 1);
+        System.out.print(a[k - 1]);
+    }
+    
+    public static void quickSort(int a[], int s, int e, int k) {
+        if(s < e) {
+            int p = partition(a, s, e);
+            if(k == p) {
+                return;
+            }else if(k < p) {
+                quickSort(a, s, p - 1, k);
+            }else {
+                quickSort(a, p + 1, e, k);
+            }
+        }
+    }
+    
+    public static int partition(int a[], int s, int e) {
+        if(s + 1 == e) {
+            if(a[s] > a[e]) {
+                swap(a, s, e);
+            }
+            return e;
+        }
+        
+        int m = (s + e) / 2;
+        
+        swap(a, s, m);
+        int p = a[s];
+        int i = s + 1;
+        int j = e;
+        while(i <= j) {
+            while(a[j] > p && j >= s + 1) {
+                j--;
+            }
+            while(a[i] < p && i <= e) {
+                i++;
+            }
+            if(i <= j) {
+                swap(a, i++, j--);
+            }
+        }
+        
+        a[s] = a[j];
+        a[j] = p;
+        return j;
+    }
+    
+    public static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp; 
+    }
 }
